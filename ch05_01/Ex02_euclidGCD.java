33a6ae3674aa51b52098489b2e070770b94bc4a0
jungyunmok <jungyunmok@gmail.com>
2024-08-05 22:01:33 +0900

    ch05-1 240805

---
+++ ch05_01/Ex02_euclidGCD.java
@@ -0,0 +1,34 @@
+package ch05_01;
+
+import java.util.Scanner;
+
+// 유클리드 호제법으로 최대공약수를 구함
+public class Ex02_euclidGCD {
+    // 정수 x, y의 최대공약수를 구하여 반환
+    static int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("두 정수의 최대공약수를 구합니다.");
+
+        System.out.print("정수를 입력하세요: ");
+        int x = scanner.nextInt();
+        System.out.print("정수를 입력하세요: ");
+        int y = scanner.nextInt();
+
+        System.out.println("최대공약수는 " + gcd(x, y) + " 입니다.");
+    }
+}
+/*
+두 정수의 최대공약수를 구합니다.
+정수를 입력하세요: 22
+정수를 입력하세요: 8
+최대공약수는 2 입니다.
+ */
